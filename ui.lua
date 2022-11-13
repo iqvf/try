d9c1503b11d18b6ca6b3b9171d2174cfd581bedc
iqvf <118130410+iqvf@users.noreply.github.com>
2022-11-13 18:41:55 +0000

    Create ui.lua

---
+++ ui.lua
@@ -0,0 +1,24 @@
+ModIDS = {
+    1,
+}
+function swagnames()
+	for _,Player in pairs(game:GetService('Players'):GetChildren()) do
+		if table.find(ModIDS, Player.UserId) then
+			if Player.Character then
+				if Player.Character.Parent.Name == 'Players' then
+					Player.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[⭐]' .. Player.DisplayName)
+				end
+			end
+		if
+			Player.Character then
+			if Player.Character.Parent.Name == 'Players' then
+				if not Player.Character.UpperTorso:FindFirstChild('OriginalSize') then
+					Player.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[👀]' .. Player.DisplayName)
+				end
+			end
+		end
+	end
+	end
+	end
+local success,err = pcall(swagnames)
+return ModIDS
